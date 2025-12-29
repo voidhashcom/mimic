98c98379ec39b39dd7013af90843f0aac89e3d40
Dominik Vít <slipiklp@gmail.com>
2025-12-29 16:27:39 +0100

    wip

--- examples/mimic-example-server/src/app.ts
+++ examples/mimic-example-server/src/app.ts
@@ -1,8 +1,7 @@
-import { Effect, Layer, Schema } from "effect";
-import { MimicServer, MimicAuthService } from "@voidhash/mimic-effect";
+import { Console, Effect, Layer } from "effect";
+import { MimicServer, MimicAuthService, MimicConfig } from "@voidhash/mimic-effect";
 import { MimicExampleSchema, PresenceSchema } from "@voidhash/mimic-example-shared";
 import { HttpLayerRouter } from "@effect/platform";
-import { Presence } from "@voidhash/mimic";
 
 // Custom auth layer - allows all tokens
 const CustomAuthLayer = MimicAuthService.layer({
@@ -12,35 +11,58 @@ const CustomAuthLayer = MimicAuthService.layer({
   }),
 });
 
+class SomeTestService extends Effect.Service<SomeTestService>()("app/SomeTestService", {
+  // Define how to create the service
+  effect: Effect.gen(function* () {
+    const hello = (name: string) => Effect.gen(function* () {
+      yield* Console.log(`Hello ${name}!`);
+      return name;
+    });
+    return { hello } as const;
+  }),
+  // Specify dependencies
+  dependencies: []
+}) {}
+
 
 // Create the Mimic route for HttpLayerRouter with custom auth and presence
-const MimicRoute = MimicServer.layerHttpLayerRouter({
+const MimicRoute = MimicServer.layerHttpLayerRouter(Effect.gen(function* () {
+  const someRandomService = yield* SomeTestService;
+  return {
   basePath: "/mimic/todo",
   schema: MimicExampleSchema,
-  authLayer: CustomAuthLayer,
   presence: PresenceSchema,
-  initial: {
-    type: "board",
-    name: "My Board",
-    children: [
-      {
-        type: "column",
-        name: "Todo",
-        children: [],
-      },
-      {
-        type: "column",
-        name: "In Progress",
-        children: [],
-      },
-      {
-        type: "column",
-        name: "Done",
-        children: [],
-      }
-    ],
-  }
-});
+  authLayer: CustomAuthLayer,
+  // Initial function that uses SomeTestService
+  initial: (ctx: MimicConfig.InitialContext) => Effect.gen(function* () {
+    console.log("Initial function called with documentId:", ctx.documentId);
+    console.log("SomeTestService:");
+    const name = yield* someRandomService.hello(ctx.documentId);
+    return {
+      type: "board" as const,
+      name: name,
+      children: [
+        {
+          type: "column" as const,
+          name: "Todo",
+          children: [],
+        },
+        {
+          type: "column" as const,
+          name: "In Progress",
+          children: [],
+        },
+        {
+          type: "column" as const,
+          name: "Done",
+          children: [],
+        }
+      ],
+    }
+  }),
+}}))
+
+ 
 
 const AllRoutes = Layer.mergeAll(
   MimicRoute
@@ -50,7 +72,9 @@ const AllRoutes = Layer.mergeAll(
       allowedOrigins: ['http://localhost:3000'],
       credentials: true
     })
-  )
+  ),
+  // Provide the service used by the initial function
+  Layer.provide(SomeTestService.Default)
 );
 
 export const AppLive = HttpLayerRouter.serve(AllRoutes);
